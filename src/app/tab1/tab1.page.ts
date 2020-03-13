7bba2b7dccaa03273ebbde97791aca1ffce7c63c
KauanPeres <50154037+KauanPeres@users.noreply.github.com>
2020-03-13 20:38:38 -0300

    Inclusão da API

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { UserService } from '../servicos/user.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  //Para paginação
+  public page = 1
+  public totalPaginas = 0;
+
+  //Mostrar informações para o usuário
+  public totalUsuarios = 0;
+  public usuariosPagina = 0;
+
+  //Para guardar a lista de usuários
+  public listaUsuarios = [];
+
+  constructor(private userService: UserService) {
+    this.buscarUsuarios(this.page);
+  }
+
+  public buscarUsuarios(pagina: Number){
+    this.userService.listaUsuarios(pagina).subscribe(dados => {
+
+      this.page = dados['page'];
+      this.totalPaginas = dados['total_pages'];
+
+      this.usuariosPagina = dados['per_page'];
+      this.totalUsuarios = dados['total'];
+
+      this.listaUsuarios = dados['data'];
+    });
+  }
+
+
+}
